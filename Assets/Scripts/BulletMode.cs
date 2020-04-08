35ae4a1fe732763a79d27cb52e49e24a6aa66522
Kobe Bogaerts <kobe.bogaerts@student.ap.be>
2020-04-08 16:40:40 +0200

    initial commit
    basis schooting geïmplementeerd in een testing scene

---
+++ Assets/Scripts/BulletMode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletMode : MonoBehaviour
+{
+    public Image image;
+    public Sprite fullMode;
+    public Sprite singleMode;
+
+    public void ToggleMode()
+    {
+        if(image.sprite.name == fullMode.name)
+        {
+            image.sprite = singleMode;
+        }
+        else
+        {
+            image.sprite = fullMode;
+        }
+    }
+}
