a8e0c742962f9ec63980b033ca69c4264f31bc8b
Kobe Bogaerts <kobe.bogaerts@student.ap.be>
2020-04-13 15:36:41 +0200

    flashlight mechanics toegevoegd en steve toegevoegd
    imorted assets en script/scene cleanup

---
+++ Assets/Scripts/PlayerClick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerClick : MonoBehaviour
+{
+
+    public Flashlight_PRO flashlight;
+    private bool isOn = false;
+    private float time = 0;
+
+    void Start()
+    {
+        flashlight.Change_Intensivity(100f);
+        flashlight.Enable_Particles(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0) && Time.time > time + 0.3f)
+        {
+            isOn = !isOn;
+            flashlight.Enable_Particles(false);
+            flashlight.Switch();
+            time = Time.time;
+        }
+    }
+}
