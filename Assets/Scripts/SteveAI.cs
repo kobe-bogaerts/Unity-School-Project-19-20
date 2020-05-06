f4c67af82616364d7c9feb4b21b062209a192940
Kobe Bogaerts Desk <kolllor3@gmail.com>
2020-05-06 13:59:37 +0200

    steve ai toegevoegd
    Hiermee kan steve naar een gameobject bewegen. De player kan ook steve freezen door met de flashlight naar steve
    te schijnen

---
+++ Assets/Scripts/SteveAI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SteveAI : MonoBehaviour
+{
+    public Transform playerPos;
+    public NavMeshAgent agent;
+    public float searchInterval = 5f;
+    public float flashFreezeDistance = 4.5f;
+    public GameObject flashLightObject;
+    public FreezeAI freezeAI;
+
+    float timer = 0.0f;
+    void Update()
+    {
+        if (timer > searchInterval)
+        {
+            agent.SetDestination(playerPos.position);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+
+        //check if light is on
+        if (flashLightObject.activeSelf)
+        {
+            if (Vector3.Distance(transform.position, flashLightObject.transform.position) < flashFreezeDistance)
+            {
+                print("Steve freeze");
+                freezeAI.Freeze();
+            }
+        }
+    }
+}
