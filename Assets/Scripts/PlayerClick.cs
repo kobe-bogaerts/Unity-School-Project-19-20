4eefb25dfa1e8e11aa43e67200950a485724a61c
Kobe Bogaerts Desk <kolllor3@gmail.com>
2020-05-14 15:37:52 +0200

    Added flashlight charge and hunting mode when steve is close

--- Assets/Scripts/PlayerClick.cs
+++ Assets/Scripts/PlayerClick.cs
@@ -6,25 +6,47 @@ public class PlayerClick : MonoBehaviour
 {
 
     public Flashlight_PRO flashlight;
+    public float flashLightChargeIncrements = 0.05f;
+    public float flashLightChargeDecrements = 0.15f;
+    public float flashLightChargeDecreseTime = 1f;
+    public Slider batterySlider;
     private bool isOn = false;
-    private float time = 0;
+    private float clickRate = 0;
+    private float timer = 0;
 
     void Start()
     {
         flashlight.Change_Intensivity(100f);
         flashlight.Enable_Particles(false);
+        batterySlider.value = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButton(0) && Time.time > time + 0.3f)
+        if (Input.GetMouseButton(0) && Time.time > clickRate + 0.3f && batterySlider.value > 0)
         {
             isOn = !isOn;
             flashlight.Enable_Particles(false);
             flashlight.Switch();
-            time = Time.time;
+            clickRate = Time.time;
+        }
+
+        if (timer > flashLightChargeDecreseTime)
+        {
+            if(isOn)
+                batterySlider.value -= flashLightChargeDecrements;
+            else
+                batterySlider.value += flashLightChargeIncrements;
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+
+        if (batterySlider.value == 0 && isOn)
+        {
+            isOn = false;
+            flashlight.Switch();
         }
     }
 }
--- Assets/Scripts/SteveAI.cs
+++ Assets/Scripts/SteveAI.cs
@@ -7,6 +7,7 @@ public class SteveAI : MonoBehaviour
     public NavMeshAgent agent;
     public float searchInterval = 5f;
     public float flashFreezeDistance = 4.5f;
+    public float huntDistance = 5f;
     public GameObject flashLightObject;
     public FreezeAI freezeAI;
 
@@ -19,6 +20,11 @@ public class SteveAI : MonoBehaviour
             timer = 0;
         }
         timer += Time.deltaTime;
+
+        if(Vector3.Distance(transform.position, playerPos.position) < huntDistance)
+        {
+            agent.SetDestination(playerPos.position);
+        }
 
         //check if light is on
         if (flashLightObject.activeSelf)
