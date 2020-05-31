d2ed92d1700e3d7bbf1134497c8b5ef02ae8ec91
Kobe Bogaerts Desk <kolllor3@gmail.com>
2020-05-31 16:36:07 +0200

    wrench kan opgepakt worden, er is nu een finish methode
    pickup system toegevoegd
    finish script toegevoegd

---
+++ Assets/Scripts/Finished.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finished : MonoBehaviour
+{
+    public PlayerDead playerState;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            if (playerState.HasWrench)
+            {
+                print("You have fixed the car safely");
+            }
+            else
+            {
+                print("You don't have the tools yet to fix me");
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerDead.cs
+++ Assets/Scripts/PlayerDead.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PlayerDead : MonoBehaviour
 {
+    private bool hasWrench;
+    public bool HasWrench { get { return hasWrench; } }
   public void Kill()
     {
         print("player is dead");
         // do something 
     }
+
+  public void setWrench(bool gotWrench)
+    {
+        this.hasWrench = gotWrench;
+    }
+    
 }
